76822c1489f6930b547ded01de327834facd556d
Matija Sabljak <matija.sabljak@gmail.com>
2017-11-04 17:23:52 +0100

    hello world

--- App.js
+++ App.js
@@ -19,19 +19,13 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+import Main from ".src/main";
+
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+          <Main/>
       </View>
     );
   }
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class Main extend React.Component {
+  render(){
+    return (
+      <View>
+          <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+export default Main;
